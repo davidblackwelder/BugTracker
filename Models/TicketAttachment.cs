dd26119b2214341f3a6ca052454ab37a26e4945c
David Blackwelder <github@davidblackwelder.com>
2022-07-29 12:39:42 -0400

    Create TicketAttachment model

---
+++ Models/TicketAttachment.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BugTracker.Models
+{
+    public class TicketAttachment
+    {
+        // Primary key
+        public int Id { get; set; }
+
+        // Foreign keys
+        [Display(Name = "Ticket")]
+        public int TicketId { get; set; }
+
+        [Display(Name = "Team Member")]
+        public string UserId { get; set; }
+
+        // Ticket Attachment fields
+        [Display(Name = "File Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [Display(Name = "File Description")]
+        public string Description { get; set; }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile FormFile { get; set; }
+
+        [Display(Name = "File Name")]
+        public string FileName { get; set; }
+
+        public byte[] FileData { get; set; }
+
+        [Display(Name = "File Extension")]
+        public string FileContentType { get; set; }
+
+        // Navigation properties
+        public virtual Ticket Ticket { get; set; }
+        public virtual BTUser User { get; set; }
+    }
+}
