8a9ff5a509d0c31f906e689129760525d2af4c3e
David Blackwelder <github@davidblackwelder.com>
2022-07-29 11:35:11 -0400

    Create TicketType model

---
+++ Models/TicketType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.Models
+{
+    public class TicketType
+    {
+        // Primary key
+        public int Id { get; set; }
+
+        [Display(Name = "Type Name")]
+        public string Name { get; set; }
+    }
+}
