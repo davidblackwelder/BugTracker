ce04870bb32ceb390ac8d1ee60664e0e12147e38
David Blackwelder <github@davidblackwelder.com>
2022-07-29 11:52:18 -0400

    Create TicketStatus model

---
+++ Models/TicketStatus.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.Models
+{
+    public class TicketStatus
+    {
+        // Primary key
+        public int Id { get; set; }
+
+        [Display(Name = "Status Name")]
+        public string Name { get; set; }
+    }
+}
