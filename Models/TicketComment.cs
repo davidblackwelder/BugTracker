6c775beb0a7b9e172289938a4f22af395a0a6bb5
David Blackwelder <github@davidblackwelder.com>
2022-07-29 13:02:53 -0400

    Create TicketComment model

---
+++ Models/TicketComment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.Models
+{
+    public class TicketComment
+    {
+        // Primary key
+        public int Id { get; set; }
+
+        // Foreign keys
+        [Display(Name = "Ticket")]
+        public int TicketId { get; set; }
+
+        [Display(Name = "Team Member")]
+        public string UserId { get; set; }
+
+        // Ticket Comment fields
+        [Display(Name = "Member Comment")]
+        public string Comment { get; set; }
+
+        [Display(Name = "Date")]
+        public DateTimeOffset Created { get; set; }
+
+        // Navigation properties
+        public virtual Ticket Ticket { get; set; }
+        public virtual BTUser User { get; set; }
+    }
+}
