18759124f618261cf86d672921b24225ac43f0a4
David Blackwelder <github@davidblackwelder.com>
2022-07-29 20:45:20 -0400

    Create Company model

---
+++ Models/Company.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.Models
+{
+    public class Company
+    {
+        // Primary key
+        public int Id { get; set; }
+
+        [Display(Name = "Company Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Company Description")]
+        public string Description { get; set; }
+
+        // Navigation properties
+        public virtual ICollection<BTUser> Members { get; set; } = new HashSet<BTUser>();
+
+        public virtual ICollection<Project> Projects { get; set; } = new HashSet<Project>();
+    }
+}
