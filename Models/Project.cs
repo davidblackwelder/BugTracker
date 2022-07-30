e08de77a2d05c9fe11de3c6db8feee7c8aae9855
David Blackwelder <github@davidblackwelder.com>
2022-07-29 20:37:07 -0400

    Create Project and ProjectPriority models

---
+++ Models/Project.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BugTracker.Models
+{
+    public class Project
+    {
+        // Primary key
+        public int Id { get; set; }
+
+        // Foreign keys
+        [Display(Name = "Company")]
+        public int? CompanyId { get; set; }
+
+        [Display(Name = "Priority")]
+        public int? ProjectPriorityId { get; set; }
+
+        // Project fields
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Project Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Start Date")]
+        public DateTimeOffset StartDate { get; set; }
+
+        [Display(Name = "End Date")]
+        public DateTimeOffset EndDate { get; set; }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile ImageFormFile { get; set; }
+
+        [Display(Name = "File Name")]
+        public string ImageFileName { get; set; }
+
+        public byte[] ImageFileData { get; set; }
+
+        [Display(Name = "File Extension")]
+        public string ImageContentType { get; set; }
+
+        [Display(Name = "Archived")]
+        public bool Archived { get; set; }
+
+        // Navigaton properties
+        public virtual Company Company { get; set; }
+
+        public virtual ProjectPriority ProjectPriority { get; set; }
+
+        public virtual ICollection<BTUser> Members { get; set; } = new HashSet<BTUser>();
+
+        public virtual ICollection<Ticket> Tickets { get; set; } = new HashSet<Ticket>();
+
+
+    }
+}
---
+++ Models/ProjectPriority.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.Models
+{
+    public class ProjectPriority
+    {
+        // Primary key
+        public int Id { get; set; }
+
+        [Display(Name = "Priority Name")]
+        public string Name { get; set; }
+    }
+}
