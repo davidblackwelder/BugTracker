7bca38ff76d6722ea7975fea8f30b7c061c48f0d
David Blackwelder <github@davidblackwelder.com>
2022-08-03 07:56:46 -0400

    Create TicketHistory service

--- Startup.cs
+++ Startup.cs
@@ -44,6 +44,7 @@ namespace BugTracker
             services.AddScoped<IBTCompanyInfoService, BTCompanyInfoService>();
             services.AddScoped<IBTProjectService, BTProjectService>();
             services.AddScoped<IBTTicketService, BTTicketService>();
+            services.AddScoped<IBTTicketHistoryService, BTTicketHistoryService>();
 
             services.AddControllersWithViews();
         }
