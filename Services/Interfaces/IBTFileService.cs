1200f0efb813e0cdf98a6fc8d10d43dbc9e621b2
David Blackwelder <github@davidblackwelder.com>
2022-08-04 07:52:41 -0400

    Create FileService

---
+++ Services/Interfaces/IBTFileService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace BugTracker.Services.Interfaces
+{
+    public interface IBTFileService
+    {
+        public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+
+        public string ConvertByteArrayToFile(byte[] fileData, string extension);
+
+        public string GetFileIcon(string file);
+
+        public string FormatFileSize(long bytes);
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -48,6 +48,7 @@ namespace BugTracker
             services.AddScoped<IBTTicketHistoryService, BTTicketHistoryService>();
             services.AddScoped<IBTNotificationService, BTNotificationService>();
             services.AddScoped<IBTInviteService, BTInviteService>();
+            services.AddScoped<IBTFileService, BTFileService>();
 
             services.AddScoped<IEmailSender, BTEmailService>();
             services.Configure<MailSettings>(Configuration.GetSection("MailSettings"));
