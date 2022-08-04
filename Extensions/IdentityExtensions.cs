09936d49bf9ecfabd611e143d14a4c60a10d9290
David Blackwelder <github@davidblackwelder.com>
2022-08-04 14:06:08 -0400

    Add UserClaims

---
+++ Extensions/IdentityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+
+namespace BugTracker.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static int? GetCompanyId(this IIdentity identity)
+        {
+            Claim claim = ((ClaimsIdentity)identity).FindFirst("CompanyId");
+
+            // Ternary operator (if/else)
+            return (claim != null) ? int.Parse(claim.Value) : null;
+        }
+    }
+}
